9be46d2a07358f6a2338d8fd6a42092397f41064
Adelmo Maximo <adelmo.maximo@conecthus.org.br>
2025-08-27 10:38:35 -0400

    feat: add project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Projeto client</h1>
+        <p>Este frontend está rodando dentro de um contêiner Docker no GKE.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
